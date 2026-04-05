87ef68cfb6185a74af07ca9a9fec3e70638ae48d
Erlon Matheus <erlonsttpro2018@gmail.comgit config --global user.name Erlon>
2026-04-05 00:59:53 -0300

    fix: remoção do banco de dados

--- backend/database/connections.ts
+++
@@ -1,8 +0,0 @@
-import * as mysql from 'mysql2/promise';
-
-export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'app',        // ou root se não criou usuário
-  password: '1234',   // sua senha
-  database: 'controle_atendimento'
-});
